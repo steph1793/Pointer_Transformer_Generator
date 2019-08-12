c562040f83a8a58b90875d4918a477043a86fe12
David Stephane <bdavidstephane@hotmail.com>
2019-08-13 01:09:30 +0200

    bug fix

--- main.py
+++ main.py
@@ -8,27 +8,27 @@ def main():
 	tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
 
 	parser = argparse.ArgumentParser()
-	parser.add_argument("--max_enc_len", default=400, help="Encoder input max sequence length")
+	parser.add_argument("--max_enc_len", default=400, help="Encoder input max sequence length", type=int)
 	
-	parser.add_argument("--max_dec_len", default=100, help="Decoder input max sequence length")
+	parser.add_argument("--max_dec_len", default=100, help="Decoder input max sequence length", type=int)
 	
-	parser.add_argument("--batch_size", default=16, help="batch size")
+	parser.add_argument("--batch_size", default=16, help="batch size", type=int)
 	
-	parser.add_argument("--vocab_size", default=50000, help="Vocabulary size")
+	parser.add_argument("--vocab_size", default=50000, help="Vocabulary size", type=int)
 	
-	parser.add_argument("--num_layers", default=3, help="Model encoder and decoder number of layers")
+	parser.add_argument("--num_layers", default=3, help="Model encoder and decoder number of layers", type=int)
 	
-	parser.add_argument("--model_depth", default=512, help="Model Embedding size")
+	parser.add_argument("--model_depth", default=512, help="Model Embedding size", type=int)
 	
-	parser.add_argument("--num_heads", default=8, help="Multi Attention number of heads")
+	parser.add_argument("--num_heads", default=8, help="Multi Attention number of heads", type=int)
 	
-	parser.add_argument("--dff", default=2048, help="Dff")
+	parser.add_argument("--dff", default=2048, help="Dff", type=int)
 
-	parser.add_argument("--seed", default=123, help="Seed")
+	parser.add_argument("--seed", default=123, help="Seed", type=int)
 	
-	parser.add_argument("--log_step_count_steps", default=1, help="Log each N steps")
+	parser.add_argument("--log_step_count_steps", default=1, help="Log each N steps", type=int)
 	
-	parser.add_argument("--max_steps",default=230000, help="Max steps for training")
+	parser.add_argument("--max_steps",default=230000, help="Max steps for training", type=int)
 		
 	parser.add_argument("--save_summary_steps", default=10000, help="Save summaries every N steps", type=int)
 	
@@ -45,6 +45,7 @@ def main():
 	
 	args = parser.parse_args()
 	params = vars(args)
+	print(params)
 
 	assert params["mode"], "mode is required. train, test or eval option"
 	if params["mode"] == "train":
